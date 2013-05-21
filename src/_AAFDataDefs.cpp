d49ee6ac03b3e57f659b03e5fe7d513c0c614793
Mark <mindmark@gmail.com>
2013-05-21 14:52:49 -0700

    added scope to DataDefs and EssenceFormats

--- src/_AAFDataDefs.cpp
+++ src/_AAFDataDefs.cpp
@@ -9,20 +9,27 @@
 // Using =======================================================================
 using namespace boost::python;
 
+// Declarations ================================================================
+
+class DataDef {};
+
+
 // Module ======================================================================
 void Export_pyste_src_AAFDataDefs()
 {
-    scope().attr("kAAFDataDef_Picture") = kAAFDataDef_Picture;
-    scope().attr("kAAFDataDef_LegacyPicture") = kAAFDataDef_LegacyPicture;
-    scope().attr("kAAFDataDef_Matte") = kAAFDataDef_Matte;
-    scope().attr("kAAFDataDef_PictureWithMatte") = kAAFDataDef_PictureWithMatte;
-    scope().attr("kAAFDataDef_Sound") = kAAFDataDef_Sound;
-    scope().attr("kAAFDataDef_LegacySound") = kAAFDataDef_LegacySound;
-    scope().attr("kAAFDataDef_Timecode") = kAAFDataDef_Timecode;
-    scope().attr("kAAFDataDef_LegacyTimecode") = kAAFDataDef_LegacyTimecode;
-    scope().attr("kAAFDataDef_Edgecode") = kAAFDataDef_Edgecode;
-    scope().attr("kAAFDataDef_DescriptiveMetadata") = kAAFDataDef_DescriptiveMetadata;
-    scope().attr("kAAFDataDef_Auxiliary") = kAAFDataDef_Auxiliary;
-    scope().attr("kAAFDataDef_Unknown") = kAAFDataDef_Unknown;
+
+    scope DataDef_scope = class_<DataDef>("DataDef");
+    DataDef_scope.attr("Picture") = kAAFDataDef_Picture;
+    DataDef_scope.attr("LegacyPicture") = kAAFDataDef_LegacyPicture;
+    DataDef_scope.attr("Matte") = kAAFDataDef_Matte;
+    DataDef_scope.attr("PictureWithMatte") = kAAFDataDef_PictureWithMatte;
+    DataDef_scope.attr("Sound") = kAAFDataDef_Sound;
+    DataDef_scope.attr("LegacySound") = kAAFDataDef_LegacySound;
+    DataDef_scope.attr("Timecode") = kAAFDataDef_Timecode;
+    DataDef_scope.attr("LegacyTimecode") = kAAFDataDef_LegacyTimecode;
+    DataDef_scope.attr("Edgecode") = kAAFDataDef_Edgecode;
+    DataDef_scope.attr("DescriptiveMetadata") = kAAFDataDef_DescriptiveMetadata;
+    DataDef_scope.attr("Auxiliary") = kAAFDataDef_Auxiliary;
+    DataDef_scope.attr("Unknown") = kAAFDataDef_Unknown;
 }
 
--- src/_AAFEssenceFormats.cpp
+++ src/_AAFEssenceFormats.cpp
@@ -9,52 +9,59 @@
 // Using =======================================================================
 using namespace boost::python;
 
+// Declarations ================================================================
+
+class EssenceFormatDef {};
+
+
 // Module ======================================================================
 void Export_pyste_src_AAFEssenceFormats()
 {
-    scope().attr("kAAFCDCIPadBits") = kAAFCDCIPadBits;
-    scope().attr("kAAFSampleRate") = kAAFSampleRate;
-    scope().attr("kAAFMaxSampleBytes") = kAAFMaxSampleBytes;
-    scope().attr("kAAFImageAlignmentFactor") = kAAFImageAlignmentFactor;
-    scope().attr("kAAFCompression") = kAAFCompression;
-    scope().attr("kAAFFieldEndOffset") = kAAFFieldEndOffset;
-    scope().attr("kAAFPhysicalTrackNum") = kAAFPhysicalTrackNum;
-    scope().attr("kAAFPixelFormat") = kAAFPixelFormat;
-    scope().attr("kAAFCDCIColorRange") = kAAFCDCIColorRange;
-    scope().attr("kAAFResolutionID") = kAAFResolutionID;
-    scope().attr("kAAFLegacyCDCI") = kAAFLegacyCDCI;
-    scope().attr("kAAFNumThreads") = kAAFNumThreads;
-    scope().attr("kAAFStoredRect") = kAAFStoredRect;
-    scope().attr("kAAFAudioSampleBits") = kAAFAudioSampleBits;
-    scope().attr("kAAFCDCIColorSiting") = kAAFCDCIColorSiting;
-    scope().attr("kAAFDisplayRect") = kAAFDisplayRect;
-    scope().attr("kAAFFieldStartOffset") = kAAFFieldStartOffset;
-    scope().attr("kAAFPixelSize") = kAAFPixelSize;
-    scope().attr("kAAFWillTransferLines") = kAAFWillTransferLines;
-    scope().attr("kAAFIsCompressed") = kAAFIsCompressed;
-    scope().attr("kAAFEssenceElementKey") = kAAFEssenceElementKey;
-    scope().attr("kAAFCompressionQuality") = kAAFCompressionQuality;
-    scope().attr("kAAFSampleFormat") = kAAFSampleFormat;
-    scope().attr("kAAFAlphaTransparency") = kAAFAlphaTransparency;
-    scope().attr("kAAFFrameLayout") = kAAFFrameLayout;
-    scope().attr("kAAFSampledRect") = kAAFSampledRect;
-    scope().attr("kAAFNumChannels") = kAAFNumChannels;
-    scope().attr("kAAFCDCIBlackLevel") = kAAFCDCIBlackLevel;
-    scope().attr("kAAFBufferLayout") = kAAFBufferLayout;
-    scope().attr("kAAFCDCIWhiteLevel") = kAAFCDCIWhiteLevel;
-    scope().attr("kAAFCDCICompWidth") = kAAFCDCICompWidth;
-    scope().attr("kAAFRGBPalette") = kAAFRGBPalette;
-    scope().attr("kAAFCDCIHorizSubsampling") = kAAFCDCIHorizSubsampling;
-    scope().attr("kAAFRGBPaletteLayout") = kAAFRGBPaletteLayout;
-    scope().attr("kAAFGamma") = kAAFGamma;
-    scope().attr("kAAFAspectRatio") = kAAFAspectRatio;
-    scope().attr("kAAFVideoLineMap") = kAAFVideoLineMap;
-    scope().attr("kAAFLegacyAUIDs") = kAAFLegacyAUIDs;
-    scope().attr("kAAFLineLength") = kAAFLineLength;
-    scope().attr("kAAFRGBCompSizes") = kAAFRGBCompSizes;
-    scope().attr("kAAFRGBCompLayout") = kAAFRGBCompLayout;
-    scope().attr("kAAFPadBytesPerRow") = kAAFPadBytesPerRow;
-    scope().attr("kAAFRGBPaletteSizes") = kAAFRGBPaletteSizes;
-    scope().attr("kAAFFieldDominance") = kAAFFieldDominance;
+
+    scope EssenceFormatDef_scope = class_<EssenceFormatDef>("EssenceFormatDef");
+    EssenceFormatDef_scope.attr("CDCIPadBits") = kAAFCDCIPadBits;
+    EssenceFormatDef_scope.attr("SampleRate") = kAAFSampleRate;
+    EssenceFormatDef_scope.attr("MaxSampleBytes") = kAAFMaxSampleBytes;
+    EssenceFormatDef_scope.attr("ImageAlignmentFactor") = kAAFImageAlignmentFactor;
+    EssenceFormatDef_scope.attr("Compression") = kAAFCompression;
+    EssenceFormatDef_scope.attr("FieldEndOffset") = kAAFFieldEndOffset;
+    EssenceFormatDef_scope.attr("PhysicalTrackNum") = kAAFPhysicalTrackNum;
+    EssenceFormatDef_scope.attr("PixelFormat") = kAAFPixelFormat;
+    EssenceFormatDef_scope.attr("CDCIColorRange") = kAAFCDCIColorRange;
+    EssenceFormatDef_scope.attr("ResolutionID") = kAAFResolutionID;
+    EssenceFormatDef_scope.attr("LegacyCDCI") = kAAFLegacyCDCI;
+    EssenceFormatDef_scope.attr("NumThreads") = kAAFNumThreads;
+    EssenceFormatDef_scope.attr("StoredRect") = kAAFStoredRect;
+    EssenceFormatDef_scope.attr("AudioSampleBits") = kAAFAudioSampleBits;
+    EssenceFormatDef_scope.attr("CDCIColorSiting") = kAAFCDCIColorSiting;
+    EssenceFormatDef_scope.attr("DisplayRect") = kAAFDisplayRect;
+    EssenceFormatDef_scope.attr("FieldStartOffset") = kAAFFieldStartOffset;
+    EssenceFormatDef_scope.attr("PixelSize") = kAAFPixelSize;
+    EssenceFormatDef_scope.attr("WillTransferLines") = kAAFWillTransferLines;
+    EssenceFormatDef_scope.attr("IsCompressed") = kAAFIsCompressed;
+    EssenceFormatDef_scope.attr("EssenceElementKey") = kAAFEssenceElementKey;
+    EssenceFormatDef_scope.attr("CompressionQuality") = kAAFCompressionQuality;
+    EssenceFormatDef_scope.attr("SampleFormat") = kAAFSampleFormat;
+    EssenceFormatDef_scope.attr("AlphaTransparency") = kAAFAlphaTransparency;
+    EssenceFormatDef_scope.attr("FrameLayout") = kAAFFrameLayout;
+    EssenceFormatDef_scope.attr("SampledRect") = kAAFSampledRect;
+    EssenceFormatDef_scope.attr("NumChannels") = kAAFNumChannels;
+    EssenceFormatDef_scope.attr("CDCIBlackLevel") = kAAFCDCIBlackLevel;
+    EssenceFormatDef_scope.attr("BufferLayout") = kAAFBufferLayout;
+    EssenceFormatDef_scope.attr("CDCIWhiteLevel") = kAAFCDCIWhiteLevel;
+    EssenceFormatDef_scope.attr("CDCICompWidth") = kAAFCDCICompWidth;
+    EssenceFormatDef_scope.attr("RGBPalette") = kAAFRGBPalette;
+    EssenceFormatDef_scope.attr("CDCIHorizSubsampling") = kAAFCDCIHorizSubsampling;
+    EssenceFormatDef_scope.attr("RGBPaletteLayout") = kAAFRGBPaletteLayout;
+    EssenceFormatDef_scope.attr("Gamma") = kAAFGamma;
+    EssenceFormatDef_scope.attr("AspectRatio") = kAAFAspectRatio;
+    EssenceFormatDef_scope.attr("VideoLineMap") = kAAFVideoLineMap;
+    EssenceFormatDef_scope.attr("LegacyAUIDs") = kAAFLegacyAUIDs;
+    EssenceFormatDef_scope.attr("LineLength") = kAAFLineLength;
+    EssenceFormatDef_scope.attr("RGBCompSizes") = kAAFRGBCompSizes;
+    EssenceFormatDef_scope.attr("RGBCompLayout") = kAAFRGBCompLayout;
+    EssenceFormatDef_scope.attr("PadBytesPerRow") = kAAFPadBytesPerRow;
+    EssenceFormatDef_scope.attr("RGBPaletteSizes") = kAAFRGBPaletteSizes;
+    EssenceFormatDef_scope.attr("FieldDominance") = kAAFFieldDominance;
 }
 
