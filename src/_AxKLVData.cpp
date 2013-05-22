986f3350a2834d98cf0cda0230f0e7e3ac28351f
Mark <mindmark@gmail.com>
2013-05-21 19:50:29 -0700

    added wrapper to AxKLVData.Initialize

--- src/_AxKLVData.cpp
+++ src/_AxKLVData.cpp
@@ -10,13 +10,29 @@
 // Using =======================================================================
 using namespace boost::python;
 
+// Declarations ================================================================
+
+template < typename T >
+void Initialize_Wrapper(AxKLVData&  axKLVData,
+                  aafUID_t& key,
+                  T pValue)
+{
+    axKLVData.Initialize(key, sizeof(pValue), reinterpret_cast<aafDataBuffer_t>(&pValue) );
+    
+};
+
+
+
 // Module ======================================================================
 void Export_pyste_src_AxKLVData()
 {
 
 
     class_< AxKLVData, boost::noncopyable >("AxKLVData", init< IAAFKLVDataSP >())
-        .def("Initialize", &AxKLVData::Initialize)
+        .def("Initialize", Initialize_Wrapper<int>)
+        .def("Initialize_aafUInt8", Initialize_Wrapper<aafUInt8>)
+        .def("Initialize_aafUInt16", Initialize_Wrapper<aafUInt16>)
+        .def("Initialize_aafUInt32", Initialize_Wrapper<aafUInt32>)
         .def("GetKey", &AxKLVData::GetKey)
         .def("GetClassName", &AxObject::GetClassName)
         .def("GetDictionary", &AxObject::GetDictionary)
